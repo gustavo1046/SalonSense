48299563d3d82015e63fb8b63a5cb6170cd47cef
Gustavo <gustavo1046@hotmail.com>
2023-03-27 20:33:41 -0300

    criando classe de agendamento

---
+++ classes/Agendamento.php
@@ -0,0 +1,60 @@
+<?php
+    class Agendamento{
+        private int $id;
+        private DateTime $hora_inicio;
+        private DateTime $hora_fim;
+        private float $valor;
+        private int $status;
+        private string $servico;
+        private string $forma_pagamento;
+        private int $id_cliente;
+        private int $id_adm;
+
+        public function __construct(int $id, DateTime $hora_i, DateTime $hora_f, float $valor, int $status, string $servico, string $forma_pagamento, $id_cliente, $id_adm){
+            $this->id = $id;
+            $this->hora_inicio = $hora_i;
+            $this->hora_fim = $hora_f;
+            $this->valor = $valor;
+            $this->status = $status;
+            $this->servico = $servico;
+            $this->forma_pagamento = $forma_pagamento;
+            $this->id_cliente = $id_cliente;
+            $this->id_adm = $id_adm;
+        }
+
+        public function getId() : int {
+            return $this->id;
+          }
+        
+          public function getHoraInicio() : DateTime {
+            return $this->hora_inicio;
+          }
+        
+          public function getHoraFim() : DateTime {
+            return $this->hora_fim;
+          }
+        
+          public function getValor() : float {
+            return $this->valor;
+          }
+        
+          public function getStatus() : int {
+            return $this->status;
+          }
+        
+          public function getServico() : string {
+            return $this->servico;
+          }
+        
+          public function getFormaPagamento() : string {
+            return $this->forma_pagamento;
+          }
+        
+          public function getIdCliente() : int {
+            return $this->id_cliente;
+          }
+        
+          public function getIdAdm() : int {
+            return $this->id_adm;
+          }
+    }
