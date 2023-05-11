e1fe4c31d388a7041587d23dc613efbf93bdd83f
Gustavo <gustavo1046@hotmail.com>
2023-05-10 21:59:04 -0300

    finzalizada conferencia de incidencia de agendamento em horario conflitando na action de agendamento

--- actions/action_Agendamento.php
+++ actions/action_Agendamento.php
@@ -8,55 +8,97 @@
     $hora2 = $_POST["hora_final"];
     $hora_fim = new DateTime($hora2);
     $data = $_POST["data"];
-    $dao = new agendamentoDao();
-    $confere_horario = $dao->ConsultarAgendamentoData($data);
-    $data = new DateTime($data);
+    $data_agend = new DateTime($data);
     $valor = $_POST["valor"];
     $servico = $_POST["desc"];
     $forma = $_POST["opcao"];
 
-    foreach($confere_horario as $conf):
-        echo "Nome: " . $conf->getNome_cliente();
-        echo "<br>";
-        echo "hora 1:" . $hora1;
-        echo "<br>";
-        echo "hora 2:" . $hora2;
-        echo "<br>";
-        $hora = date('H:i', $conf->getHoraInicio()->getTimestamp());
-        echo "hora inicio cliente:" . $hora;
-        echo "<br>";
-        $hora3 = date('H:i', $conf->getHoraFim()->getTimestamp());
-        echo "hora final cliente:" . $hora3;
 
-        echo "<br>";
-        if($hora_inicio->getTimestamp() >= $conf->getHoraInicio()->getTimestamp()){
-            if($hora_inicio->getTimestamp() <= $conf->getHoraFim()->getTimestamp()){
-                echo "vishi mano num vai da nao";
-                echo "<br>";
-            }else {
-                echo "ta tudo bem marcar";
-                echo "<br>";
+    $dao = new agendamentoDao();
+    $confere_horario = $dao->ConsultarAgendamentoData($data);
+    $count = 0; //variavel de contagem de incidencia no intervalo do horario
+
+    $hora_i = $hora_inicio->format('H');
+    $hora_f = $hora_fim->format('H');
+
+    if($hora_i > $hora_f){
+        echo "tu é doido é macho?"; //naaan
+    }
+    else{
+        foreach($confere_horario as $conf):
+            $hora_inicio_cliente = $conf->getHoraInicio();
+            $hora_inicio_cliente = $hora_inicio_cliente->format('H');
+            $hora_fim_cliente = $conf->getHorafim();
+            $hora_fim_cliente = $hora_fim_cliente->format('H');
+    
+            if($hora_i >= $hora_inicio_cliente){
+                if($hora_f <= $hora_fim_cliente && $hora_f >= $hora_inicio_cliente){
+                    $count += 1;
+                }
             }
-
-        } else {
-            echo "ta tudo bem marcar";
-            echo "<br>";
+        endforeach;
+    
+        $agend = new Agendamento($nome, $hora_inicio, $hora_fim, $data_agend, $valor, $servico, $forma, 1);
+        $dao->InserirAgendamento($agend);
+    
+        if($count == 0){
+            header("Location: ../Pages/Agendamento/Agendamento.php");
         }
-    endforeach; 
-
-    $agend = new Agendamento($nome, $hora_inicio, $hora_fim, $data, $valor, $servico, $forma, 1);
-    $dao->InserirAgendamento($agend);
-    // header("Location: ../Pages/Agendamento/Agendamento.php");
-    exit();
-
+        else{
+           echo  "horario de agendamento ja esta em uso";
+        }
+        exit();
+    }
 
 
-    // echo "Nome: " . $nome . "<br>";
-    // echo "Hora de Início: " . $hora_inicio->format('Y-m-d H:i:s') . "<br>";
-    // echo "Hora de Fim: " . $hora_fim->format('Y-m-d H:i:s') . "<br>";
-    // echo "Data: " . $data->format('Y-m-d') . "<br>";
-    // echo "Valor: " . $valor . "<br>";
-    // echo "Descrição do Serviço: " . $servico . "<br>";
-    // echo "Forma de Pagamento: " . $forma . "<br>";
+    
+
+
+    // TESTE DE CONFERENCIA DE INCIDENCIA DE AGENDAMENTO NO BANCO DE DADOS 
+    // (PARA CASOS DE AGENDAMENTOS EM HORARIO CONFLITANTES)
+    // colar dentro do foreach e retirar o header da pagina apra visualizar resultados
+
+    
+    // echo "Nome do banco: " . $conf->getNome_cliente();
+    // echo "<br>";
+    // echo "Nome do cliente cadastrado: ". $nome;
+    // echo "<br>";
+    // echo "hora 1:" . $hora1;
+    // echo "<br>";
+    // echo "hora 2:" . $hora2;
+    // echo "<br>";
+    // $hora = date('H:i', $conf->getHoraInicio()->getTimestamp());
+    // echo "hora inicio cliente:" . $hora;
+    // echo "<br>";
+    // $hora3 = date('H:i', $conf->getHoraFim()->getTimestamp());
+    // echo "hora final cliente:" . $hora3;
+    // echo "<br>";
+    // $hora_i = $hora_inicio->format('H');
+    // $hora_f = $hora_fim->format('H');
+    // $hora_inicio_cliente = $conf->getHoraInicio();
+    // $hora_inicio_cliente = $hora_inicio_cliente->format('H');
+    // $hora_fim_cliente = $conf->getHorafim();
+    // $hora_fim_cliente = $hora_fim_cliente->format('H');
+
+    // if($hora_i >= $hora_inicio_cliente){
+    //     if($hora_f <= $hora_fim_cliente && $hora_f >= $hora_inicio_cliente){
+    //         echo "Noa pode ser feito o cadastro";
+    //         echo "<br>";
+    //         echo "<br>";
+    //         echo "<br>";
+    //         echo "<br>";
+    //         echo "<br>";
+    //         $count += 1;
+    //     }else {
+    //         echo "ta tudo bem marcar - 2";
+    //         echo "<br>";
+    //     }
+    // }
+    // else {
+    //     echo "";
+    //     echo "ta tudo bem marcar - 1";
+    //     echo "<br>";
+    // }
+
 
 
