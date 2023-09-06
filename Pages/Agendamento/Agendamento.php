7e6c11a98c9ada4e615d2a891b1f72d8d08befa7
Gustavo <gustavo1046@hotmail.com>
2023-09-06 19:06:19 -0300

    adicionando modald e telefone na tela de agendamento ao cliente fixo

--- Pages/Agendamento/Agendamento.php
+++ Pages/Agendamento/Agendamento.php
@@ -12,6 +12,20 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.html';
 ?>
 <body>
+
+<div class="modal" id="modalExc">
+        <div class="modal-content-exc">
+          <span class="close">&times;</span>
+          <form action="../../actions/action_Agendamento.php" method="POST">
+            <div class="modal-footer">
+                <input type="tel" name="telefone" placeholder="telefone de contato" id="telefone"><br>
+                <input type="submit" value="Cadastrar cliente" id="excluir">
+            </div>      
+          </form>
+        </div>
+    </div>
+
+
     <header>
         <img src="../../assets/logo.png">
     </header>
@@ -24,8 +38,8 @@
         <input type="date" name="data" placeholder="data do atendimento" id="data" required><br>
         <input tyle="number" name="valor" placeholder="valor do serviço" id="valor" required><br>
         <input type="text" name="desc" placeholder="descrição" maxlength="150" id="desc" required><br>
-        <input type="tel" name="telefone" placeholder="telefone de contato" id="telefone"><br>
         <div class="radio">
+          <br>
           <label>
               <input type="radio" name="opcao" value="Dinheiro"  id="opcao1" checked>
               Dinheiro
@@ -42,11 +56,31 @@
           <input type="checkbox" name="opcao_cliente" id="opcao_cliente">  cliente fixo?
         </div>
         <div class="sub">
-          <input type="submit" value="Agendar" id="agendar">
+          <input type="submit" value="Agendar" id="agendar" onclick="showModalDelete()">
         </div>
         <a href="../Home Page/HomePage.php">Voltar ao inicio</a>
         <input type="hidden" name="op" id ="op" value=0>
       </form>
-    </div>
+    </div>  
 </body>
+<script>
+    var modalDelete = document.getElementById("modalExc");
+    var spanDel = document.getElementsByClassName("close")[1];
+
+    // Quando o usuário clicar no botão "fechar" ou fora do modal, feche-o
+
+    spanDel.onclick = function() {
+      modalDelete.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modalDelete) {
+        modalDelete.style.display = "none";
+      }
+
+    }
+    function showModalDelete(id){
+      modalDelete.style.display = "block";
+    }
+  </script>
 </html>
