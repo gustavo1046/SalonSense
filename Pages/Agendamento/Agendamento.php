8bd8878717d7608e66c6cb608d16bdec590d3619
Gustavo <gustavo1046@hotmail.com>
2023-09-06 21:24:48 -0300

    adicionando funcionalidade de cadastro de cliente no formualrio na tela de agenamento

--- Pages/Agendamento/Agendamento.php
+++ Pages/Agendamento/Agendamento.php
@@ -16,12 +16,10 @@
 <div class="modal" id="modalExc">
         <div class="modal-content-exc">
           <span class="close">&times;</span>
-          <form action="../../actions/action_Agendamento.php" method="POST">
             <div class="modal-footer">
-                <input type="tel" name="telefone" placeholder="telefone de contato" id="telefone"><br>
-                <input type="submit" value="Cadastrar cliente" id="excluir">
+                <input type="tel" name="telefone" placeholder="telefone de contato" id="telefone" required><br>
+                <button id="tel_button" onclick="">Cadastrar cliente</button>
             </div>      
-          </form>
         </div>
     </div>
 
@@ -30,14 +28,14 @@
         <img src="../../assets/logo.png">
     </header>
     <div class="container">
-      <form action="../../actions/action_Agendamento.php" method="POST">
-        
+      <form action="../../actions/action_Agendamento.php" method="POST" id="formulario">    
         <input type="text" name="nome" placeholder="nome da cliente" id="nome" pattern="[A-Za-z0-9 ]+" maxlength="30" required><br>
         <input type="time" name="hora_inicio" placeholder="inicio do atendimento" id="hora_inicio" required><br>
         <input type="time" name="hora_final" placeholder="final do atendimento" id="hora_final" required><br>
         <input type="date" name="data" placeholder="data do atendimento" id="data" required><br>
         <input tyle="number" name="valor" placeholder="valor do serviço" id="valor" required><br>
         <input type="text" name="desc" placeholder="descrição" maxlength="150" id="desc" required><br>
+        <input type="hidden" name="telefone" placeholder="telefone de contato" id="tel">
         <div class="radio">
           <br>
           <label>
@@ -55,32 +53,82 @@
         <div class="cliente">
           <input type="checkbox" name="opcao_cliente" id="opcao_cliente">  cliente fixo?
         </div>
+        <input type="hidden" name="op" id ="op" value=0>
         <div class="sub">
-          <input type="submit" value="Agendar" id="agendar" onclick="showModalDelete()">
+        </form>
+          <button type="button" id="agendar" onclick="clienteFixo()">Agendar</button>
         </div>
         <a href="../Home Page/HomePage.php">Voltar ao inicio</a>
-        <input type="hidden" name="op" id ="op" value=0>
-      </form>
     </div>  
 </body>
 <script>
-    var modalDelete = document.getElementById("modalExc");
-    var spanDel = document.getElementsByClassName("close")[1];
 
-    // Quando o usuário clicar no botão "fechar" ou fora do modal, feche-o
+  function verificarCampos() {
+  // Obtém o formulário pelo ID
+  var formulario = document.getElementById("formulario");
+  
+  // Obtém todos os elementos de input dentro do formulário
+  var campos = formulario.querySelectorAll("input[required]");
+  
+  // Define uma variável para rastrear se todos os campos estão preenchidos
+  var todosCamposPreenchidos = true;
 
-    spanDel.onclick = function() {
-      modalDelete.style.display = "none";
+  // Itera por todos os campos e verifica se estão preenchidos
+  for (var i = 0; i < campos.length; i++) {
+    if (campos[i].value.trim() === "") {
+      todosCamposPreenchidos = false;
+      break; // Se encontrar um campo vazio, sai do loop
+    }
+  }
+  return todosCamposPreenchidos;
+}
+
+    function clienteFixo(){
+      var tel = document.getElementById("opcao_cliente");
+      if(tel.checked){
+        showModalDelete();
+      }
+      else{
+        var verifica = verificarCampos();
+        if(verifica){
+          submitFormulario();
+        }
+        else{
+          alert("preenche saporra ai");
+        }
+      }
     }
 
-    window.onclick = function(event) {
-      if (event.target == modalDelete) {
+    function submitFormulario(){
+      var formulario = document.getElementById("formulario"); 
+      formulario.submit();
+    }
+
+    function showModalDelete(id){
+      var modalDelete = document.getElementById("modalExc");
+      var spanDel = document.getElementsByClassName("close")[1];
+
+      // Quando o usuário clicar no botão "fechar" ou fora do modal, feche-o
+
+      spanDel.onclick = function() {
         modalDelete.style.display = "none";
       }
 
-    }
-    function showModalDelete(id){
+      window.onclick = function(event) {
+        if (event.target == modalDelete) {
+          modalDelete.style.display = "none";
+        }
+
+      }
+
       modalDelete.style.display = "block";
+    }
+
+    function TelefoneValor(){
+      var tel = document.getElementById("telefone").value;
+      document.getElementById("tel").value = tel;
+      var formulario = document.getElementById("formulario"); 
+      // submitFormulario();
     }
   </script>
 </html>
